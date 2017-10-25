00e9f08183510203c8504641a6ad13048ac3aa81
leexa <lee.x.a90@gmail.com>
2017-10-25 17:48:26 +0800

    2510

---
+++ week5/NLP1.py
@@ -0,0 +1,63 @@
+import numpy as np
+import os
+import nltk
+import pandas as pd
+nltk.download('punkt')
+sentence_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+dictt={}
+def seperate(j):
+    result = []
+    prev = 0
+    for i in range(len(j)):
+                    if j[i] == ':':
+                            if (j[i-2:i]+j[i+1:i+3]).isnumeric():
+                                    result +=[ j[prev:i-2],]
+                                    prev = i-2
+    result += [j[prev:],]
+    return result
+def get_link(j):
+    result = []
+    for i in range(len(j)):
+        if j[i:i+14]=='http://str.sg/':
+            result=[j[:i],j[i:].split()[0]]
+    return result
+
+df = pd.DataFrame([[np.nan,]*5],columns=['year','date','time','data','site'])
+for file in [x for x in os.listdir('.') if '2016.txt' in x]:
+    year = '2016'
+    for line in open(file,'r'):
+        #print line
+        #x =  sentence_splitter.tokenize(line.decode('utf-8'))
+        if  '.txt' in line:
+            line = line[:-1]
+            dictt[line] = []
+            impt = line
+        else:
+            dictt[impt] =seperate(line.decode('utf-8'))[2:]
+        for a in seperate(line.decode('utf-8'))[:]:
+            None#print a
+
+    for date in dictt:
+        temp = []
+        for article in dictt[date]:
+            #print article
+            temp +=[ [year,date,article[0:5],]+get_link(article[5:]),]
+        df = df.append(pd.DataFrame(temp,columns=['year','date','time','data','site']))
+df = df.reset_index(drop=1)   
+
+df2 = pd.read_csv('ES3.SI.csv')
+
+dictt_month = {'march': '03', 'august': '08', 'may': '05', 'june': '06', 'jan': '01', 'april': '04',
+               'feb': '02', 'july': '07', 'dec': '12', 'sept': '09', 'oct': '10', 'nov': '11'}
+
+def fun(x):
+    result =  [dictt_month[x[:-4].split('_')[0]],x[:-4].split('_')[1]]
+    if len(result[1]) == 1:
+        result[1] = '0' + result[1]
+    return result
+
+df['date'][1:].apply(fun)
+df = df.iloc[1:]
+df['date2'] = df['date'][:].apply(fun)
+df['Date'] = map(lambda x :x[1]['year']+'-'+ x[1]['date2'][0]+'-'+ x[1]['date2'][1],df.iloc[:].iterrows())
+df_comb = pd.merge(df,df2,'left',on='Date')
--- week5/web_scrape.py
+++ week5/web_scrape.py
@@ -13,11 +13,14 @@ caps["marionette"] = True
 caps["pageLoadStrategy"] = "normal"  #  complete
 driver = webdriver.Firefox(capabilities=caps)
 driver.maximize_window()
+page = 'https://acc-reg.sphdigital.com/RegAuth2/sphLogin.html?svc=sts&goto=https%3A%2F%2Facc-auth.sphdigital.com%2Famserver%2Foauth2%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3Dstraitstimes_oauth%26redirect_uri%3Dhttps%253A%252F%252Fstraitstimes-mobilegateway-api-SIN-LIVE.stapi.straitstimes.com%252Fapi%252FSphToken%26state%3D%257B%2522originalUrl%2522%253A%2522http%253A%252F%252Fwww.straitstimes.com%252Fsingapore%252Fst-now-news-as-it-happens-may-1-2017%253Flogin%253Dtrue%2522%252C%2522sessionId%2522%253A968011982%252C%2522sessionToken%2522%253A%25228d934b2707924890b355e8176cb35e5e%2522%252C%2522deviceId%2522%253A%2522fa470d7d2fb6446a392d13f04a0abc63103.20.170.81%2522%252C%2522platform%2522%253A%2522web%2522%252C%2522company%2522%253A%2522sph%2522%252C%2522publication%2522%253A%2522ST%2522%252C%2522longitude%2522%253Anull%252C%2522latitude%2522%253Anull%252C%2522newlyRegistered%2522%253A%2522%2522%257D%26scope%3Daonickname%2520uid%2520sn%2520cn%2520aoregservice%2520aovisitorid%2520aologinid'
+driver.get(page)
+
 import time
-def main():
-    for year in ['2016',]:
+def main(i,):
+    for year in ['2017',]:
         for month in ['jan','feb','march','april','may','june','july','august',\
-                      'sept','oct','nov','dec'][7:]:
+                      'sept','oct','nov','dec'][i:i+1]:
             f1=open(month+year+'.txt','w')
             for i in range(1,32):
                 print (month,i)
@@ -30,7 +33,7 @@ def main():
                 P = True
                 result = ''
                 counter = 0
-                while result == '' and counter <= 3:
+                while result == '' and counter <= 5:
                     if counter >= 1:
                         time.sleep(2)
                     counter += 1
