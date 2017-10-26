aa03bc539791bf7729a9643004e918f9e3e62f00
leexa <lee.x.a90@gmail.com>
2017-10-26 09:16:30 +0800

    2610

--- week5/NLP1.py
+++ week5/NLP1.py
@@ -2,9 +2,9 @@ import numpy as np
 import os
 import nltk
 import pandas as pd
-nltk.download('punkt')
+#nltk.download('punkt')
 sentence_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
-dictt={}
+
 def seperate(j):
     result = []
     prev = 0
@@ -19,32 +19,33 @@ def get_link(j):
     result = []
     for i in range(len(j)):
         if j[i:i+14]=='http://str.sg/':
-            result=[j[:i],j[i:].split()[0]]
+            result=[j[:i],j[i:].split()[0],j[i:].split()]
     return result
 
-df = pd.DataFrame([[np.nan,]*5],columns=['year','date','time','data','site'])
-for file in [x for x in os.listdir('.') if '2016.txt' in x]:
-    year = '2016'
-    for line in open(file,'r'):
-        #print line
-        #x =  sentence_splitter.tokenize(line.decode('utf-8'))
-        if  '.txt' in line:
-            line = line[:-1]
-            dictt[line] = []
-            impt = line
-        else:
-            dictt[impt] =seperate(line.decode('utf-8'))[2:]
-        for a in seperate(line.decode('utf-8'))[:]:
-            None#print a
+df = pd.DataFrame([[np.nan,]*6],columns=['year','date','time','data','site','site2'])
+for year in ['2016','2017']:
+    for file in [x for x in os.listdir('.') if '2016.txt' in x]:
+        dictt={}
+        for line in open(file,'r'):
+            #print line
+            #x =  sentence_splitter.tokenize(line.decode('utf-8'))
+            if  '.txt' in line:
+                line = line[:-1]
+                dictt[line] = []
+                impt = line
+            else:
+                dictt[impt] =seperate(line.decode('utf-8'))[2:]
+            for a in seperate(line.decode('utf-8'))[:]:
+                None#print a
 
-    for date in dictt:
-        temp = []
-        for article in dictt[date]:
-            #print article
-            temp +=[ [year,date,article[0:5],]+get_link(article[5:]),]
-        df = df.append(pd.DataFrame(temp,columns=['year','date','time','data','site']))
+        for date in dictt:
+            temp = []
+            for article in dictt[date]:
+                #print article
+                temp +=[ [year,date,article[0:5],]+get_link(article[5:]),]
+            df = df.append(pd.DataFrame(temp,columns=['year','date','time','data','site','site2']))
 df = df.reset_index(drop=1)   
-
+die
 df2 = pd.read_csv('ES3.SI.csv')
 
 dictt_month = {'march': '03', 'august': '08', 'may': '05', 'june': '06', 'jan': '01', 'april': '04',
