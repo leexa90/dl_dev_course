1eb1a43e8b0798f9f8e8ea4dfb0081372bee2bb5
leexa <lee.x.a90@gmail.com>
2017-11-17 15:11:40 +0800

    1711

--- peptide-permable/weekend.sh
+++ peptide-permable/weekend.sh
@@ -1,28 +1,28 @@
-python CNN_layer5_avg_pooling.py 2  > cnn_layer5.log2
+python CNN_layer7_coslr.py  2  > cnn_layer7.log2
 #git pull
 #git add *
 #git commit -m 'weekend test set 2'
 #git push
 
-python CNN_layer5_avg_pooling.py 1 > cnn_layer5.log1
+python CNN_layer7_coslr.py  1 > cnn_layer7.log1
 #git pull
 #git add *
 #git commit -m 'weekend test set 1'
 #git push
 
-python CNN_layer5_avg_pooling.py 0 > cnn_layer5.log0
+python CNN_layer7_coslr.py  0 > cnn_layer7.log0
 #git pull
 #git add *
 #git commit -m 'weekend test set 0'
 #git push
 
-python CNN_layer5_avg_pooling.py 3 > cnn_layer5.log3
+python CNN_layer7_coslr.py  3 > cnn_layer7.log3
 ##git pull
 #git add *
 ##git commit -m 'weekend test set 3'
 #git push
 
-python CNN_layer5_avg_pooling.py 4 > cnn_layer5.log4
+python CNN_layer7_coslr.py  4 > cnn_layer7.log4
 #git pull
 #git add *
 #git commit -m 'weekend test set 4'
